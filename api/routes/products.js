1df5b6c15e8e699a78e37eecf8a33743d636b7e2
Shutsen <vancampenhoutgeert@gmail.com>
2019-08-09 11:41:54 +0200

    Add product model and api routes

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,50 +1,82 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+
+const Product = require('../models/product');
 
 router.get('/', (req, res, next) => {
-	res.status(200).json({
-		message: 'Handling GET requests to /products'
-	});
+	Product.find()
+		.exec()
+		.then(products => res.status(200).json({ products }))
+		.catch(err => res.status(500).json({ error: err }));
 });
 
 router.post('/', (req, res, next) => {
-	const product = {
+	const product = new Product({
+		_id: new mongoose.Types.ObjectId(),
 		name: req.body.name,
 		price: req.body.price
-	};
-
-	res.status(201).json({
-		message: 'A new product has been created! Well done man!',
-		product: product
 	});
+	product
+		.save()
+		.then(result => {
+			res.status(201).json({
+				message: 'A new product has been created! Well done man!',
+				product: product
+			});
+		})
+		.catch(err => {
+			res.status(500).json({
+				error: err
+			})
+		});
 });
 
 router.get('/:product_id', (req, res, next) => {
 	const id = req.params.product_id;
-
-	if (id === 'magic') {
-		res.status(200).json({
-			message: 'You discovered the magic product',
-			id: id
+	Product.findById(id)
+		.exec()
+		.then(product => {
+			if (product) {
+				res.status(200).json({ product });
+			} else {
+				res.status(404).json({
+					message: 'No valid product found for the provided id'
+				});
+			}
+		})
+		.catch(err => {
+			res.status(500).json({
+				error: err
+			});
 		});
-	} else {
-		res.status(200).json({
-			message: 'You discovered a regular product',
-			id: id
-		});
-	}
 });
 
 router.patch('/:product_id', (req, res, next) => {
-	res.status(200).json({
-		message: 'You updated the product'
-	});
+	console.log('connected via Patch request')
+	Product.update({ _id: req.params.product_id }, { $set: req.body })
+		.exec()
+		.then(product => {
+			if (product) {
+				res.status(200).json({ product });
+			} else {
+				res.status(404).json({
+					message: 'No valid product found for the provided id'
+				});
+			}
+		})
+		.catch(err => {
+			res.status(500).json({
+				error: err
+			});
+		});
 });
 
 router.delete('/:product_id', (req, res, next) => {
-	res.status(200).json({
-		message: 'You deleted the product'
-	});
+	Product.remove({ _id: req.params.product_id })
+		.exec()
+		.then(result => res.status(200).json({ result }))
+		.catch(err => res.status(500).json({ error: err }));
 });
 
 module.exports = router;
