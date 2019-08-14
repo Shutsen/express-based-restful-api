beb07f8d1ac0c99b881dafaa8e48c3c029c67677
Shutsen <vancampenhoutgeert@gmail.com>
2019-08-14 16:44:19 +0200

    Delete user route

--- api/routes/users.js
+++ api/routes/users.js
@@ -6,46 +6,45 @@ const bcrypt = require('bcrypt');
 const User = require('../models/user');
 
 router.post('/signup', (req, res, next) => {
-	// check if email already exists
 	User.find({ email: req.body.email })
 		.exec()
 		.then(user => {
-			if (user) {
+			// if email already exists, return ad hoc
+			if (user.length > 0) {
 				return res.status(409).json({ message: 'A user with this email address already exists.' })
 			}
-		})
-		.catch(err => json.status(500).json({ error: err }));
 
-	// hash the password and create new user
-	bcrypt.hash(req.body.password, 10, (err, hash) => {
-		if (err) {
-			return res.status(500).json({
-				error: err
-			});
-		}
+			// hash the password and create new user
+			bcrypt.hash(req.body.password, 10, (err, hash) => {
+				if (err) {
+					return res.status(500).json({
+						error: err
+					});
+				}
 
-		const user = new User({
-			_id: new mongoose.Types.ObjectId(),
-			first_name: req.body.first_name,
-			last_name: req.body.last_name,
-			email: req.body.email,
-			password: hash
-		});
+				const user = new User({
+					_id: new mongoose.Types.ObjectId(),
+					first_name: req.body.first_name,
+					last_name: req.body.last_name,
+					email: req.body.email,
+					password: hash
+				});
 
-		user
-		.save()
-		.then(result => {
-			res.status(201).json({
-				message: `Welcome, ${user.first_name}! You have signed up successfully.`,
-				user: user
-			});
-		})
-		.catch(err => {
-			res.status(500).json({
-				error: err
+				user
+				.save()
+				.then(result => {
+					res.status(201).json({
+						message: `Welcome, ${user.first_name}! You have signed up successfully.`,
+						user: user
+					});
+				})
+				.catch(err => {
+					res.status(500).json({
+						error: err
+					})
+				});
 			})
-		});
-	})
+		})
 });
 
 // GET all users and return them with the total count
@@ -59,7 +58,7 @@ router.get('/', (req, res, next) => {
 			})
 		})
 		.catch(err => res.status(500).json({ error: err }));
-})
+});
 
 
 // GET user detail
@@ -69,6 +68,14 @@ router.get('/:user_id', (req, res, next) => {
 		.exec()
 		.then(user => res.status(200).json({ user }))
 		.catch(err => res.status(500).json({ error: err }));
-})
+});
+
+// DELETE - remove a certain user
+router.delete('/:user_id', (req, res, next) => {
+	User.remove({ _id: req.params.user_id })
+		.exec()
+		.then(result => res.status(200).json({ message: 'User deleted' }))
+		.catch(err => res.status(500).json({ error: err }));
+});
 
 module.exports = router;
