3e30b51ee1dff7c43af3e04b33af98b17ba983ce
Shutsen <vancampenhoutgeert@gmail.com>
2019-08-15 12:46:32 +0200

    Capitalize controllers

--- api/routes/products.js
+++ api/routes/products.js
@@ -2,12 +2,12 @@ const express = require('express');
 const router = express.Router();
 const uploadImage = require('../../utils/upload_image');
 const checkAuth = require('../middleware/check-auth');
-const productsController = require('../controllers/products');
+const ProductsController = require('../controllers/products');
 
-router.get('/', productsController.getAll);
-router.get('/:product_id', productsController.getDetail);
-router.post('/', checkAuth, uploadImage.single('productImage'), productsController.create);
-router.patch('/:product_id', checkAuth, productsController.edit);
-router.delete('/:product_id', checkAuth, productsController.delete);
+router.get('/', ProductsController.getAll);
+router.get('/:product_id', ProductsController.getDetail);
+router.post('/', checkAuth, uploadImage.single('productImage'), ProductsController.create);
+router.patch('/:product_id', checkAuth, ProductsController.edit);
+router.delete('/:product_id', checkAuth, ProductsController.delete);
 
 module.exports = router;
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,12 +1,12 @@
 require('dotenv').config()
 const express = require('express');
 const router = express.Router();
-const usersController = require('../controllers/users');
+const UsersController = require('../controllers/users');
 
-router.get('/', usersController.getAll);
-router.get('/:user_id', usersController.getDetail);
-router.post('/signup', usersController.signup);
-router.post('/login', usersController.login);
-router.delete('/:user_id', usersController.delete);
+router.get('/', UsersController.getAll);
+router.get('/:user_id', UsersController.getDetail);
+router.post('/signup', UsersController.signup);
+router.post('/login', UsersController.login);
+router.delete('/:user_id', UsersController.delete);
 
 module.exports = router;
