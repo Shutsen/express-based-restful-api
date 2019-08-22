02557b11674c2c9e534e1b1880e17ccf62274213
Shutsen <vancampenhoutgeert@gmail.com>
2019-08-22 11:08:10 +0200

    Correct order plz

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ app.use('/uploads', express.static('uploads'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-app.use((res, req, next) => {
+app.use((req, res, next) => {
 	res.header('Access-Control-Allow-Origin', '*');
 	res.header(
 		'Access-Control-Allow-Headers',
